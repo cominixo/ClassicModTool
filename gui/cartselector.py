521215a72011a618d69bbeacffb67347c6be0cda
cominixo01 <22600756+cominixo01@users.noreply.github.com>
2021-01-12 19:21:02 -0300

    remove this

--- gui/cartselector.py
+++
@@ -1,8 +0,0 @@
-from PyQt5.QtGui import *
-from PyQt5.QtWidgets import *
-from PyQt5.QtCore import *
-
-
-class CartSelector(QWidget):
-    def __init__(self, *args, **kwargs):
-        QWidget.__init__(self, *args, **kwargs)
